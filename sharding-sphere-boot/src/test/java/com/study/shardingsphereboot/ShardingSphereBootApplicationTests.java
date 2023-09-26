87069f518641c8f02b36d71d6c378262ed0c4a9c
luohx <Lhongxiao@lcchain.com>
2023-09-26 23:24:23 +0800

    新建分库分表项目

---
+++ sharding-sphere-boot/src/test/java/com/study/shardingsphereboot/ShardingSphereBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.shardingsphereboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShardingSphereBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
